a3793ee0440d50a8a3e15ef669e47c13bb58b663
Ardhi Lukianto <ardhi@lukianto.com>
2021-02-16 16:04:53 +0700

    Updates

--- src/isCoordInBounds.js
+++ src/isCoordInBounds.js
@@ -1,3 +1,8 @@
 module.exports = function (lng, lat, bounds) {
+  if (lng.length) {
+    bounds = lat
+    lat = lng[1]
+    lng = lng[0]
+  }
   return lng >= bounds[0] && lng <= bounds[2] && lat >= bounds[1] && lat <= bounds[3]
 }
